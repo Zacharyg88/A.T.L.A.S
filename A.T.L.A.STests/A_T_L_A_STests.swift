2ecdc30bd3ada7fdeefcc4f8f821384e9e655c33
Zachary Eidenberger <zacharyg88@gmail.com>
2025-01-02 09:19:11 -0500

    added missing files, pushing to origin

---
+++ A.T.L.A.STests/A_T_L_A_STests.swift
@@ -0,0 +1,16 @@
+//
+//  A_T_L_A_STests.swift
+//  A.T.L.A.STests
+//
+//  Created by Zach Eidenberger on 12/3/24.
+//
+
+import Testing
+
+struct A_T_L_A_STests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ A.T.L.A.SUITests/A_T_L_A_SUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  A_T_L_A_SUITestsLaunchTests.swift
+//  A.T.L.A.SUITests
+//
+//  Created by Zach Eidenberger on 12/3/24.
+//
+
+import XCTest
+
+final class A_T_L_A_SUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
